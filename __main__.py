25e57ccaf54a51132d6e729084c19699396722e2
Fidel Moises Apari Sanchez <fidel.moises0@gmail.com>
2022-06-20 13:39:08 -0500

    Cambio de nombre

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
-import potencia
+import maths
 
 def inicio():
     operacion = input("Ingrese la operacion que desea realizar: ")
@@ -6,18 +6,18 @@ def inicio():
     if operacion == "P":
         p1 = int(input("Ingrese la base: "))
         p2 = int(input("Ingrese el exponente: "))
-        potencia_r = potencia.MathMatics.potencia(p1, p2)
+        potencia = maths.MathMatics.maths(p1, p2)
 
-        print(f"El resultado es {potencia_r}")
+        print(f"El resultado es {potencia}")
     elif operacion == "F":
         p1 = int(input("Ingrese el numero para la factorial: "))
-        factorial = potencia.MathMatics.factorial(p1= p1)
+        factorial = maths.MathMatics.factorial(p1= p1)
 
         print(f"El resultado es {factorial}")
 
     elif operacion == "Fi":
         p1 = int(input("Ingrese el numero de la serie que desea: "))
-        fibo = potencia.MathMatics.fibonacci(p1)
+        fibo = maths.MathMatics.fibonacci(p1)
 
         print(f"EL resultado es {fibo}")
 
