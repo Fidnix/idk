421770425271e955b4fd66770867694333d34498
Fidel Moises Apari Sanchez <fidel.moises0@gmail.com>
2022-06-20 15:13:08 -0500

    Añadiendo nuevos metodos

--- maths.py
+++ maths.py
@@ -26,3 +26,19 @@ class MathMatics:
             n2 = n3
 
         return n2
+
+    @staticmethod
+    def suma_inversa(n):
+        suma = 0
+        for i in range(1,n+1):
+            suma += 1/(i)
+
+        return suma
+
+    @staticmethod
+    def suma_bases2_inversas(n):
+        suma = 0
+        for i in range(0, n+1):
+            suma+= 1/(2**i)
+
+        return suma
